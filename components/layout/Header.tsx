99a06bed0f3799b07efb575402551fd1c2fcffa1
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-19 12:11:50 +1200

    Setup: Header for desktop layout

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,3 +1,80 @@
-export const Header = () => {
-  return <header>Header</header>;
+"use client";
+
+import { FaGithub, FaUser } from "react-icons/fa";
+import { Link as ReactScrollLink } from "react-scroll";
+import { navLinks } from "../data/navLinks";
+import { useState } from "react";
+import Link from "next/link";
+
+export const Header = ({
+  logoName,
+  gitHubAccountLink,
+  websiteVersion,
+}: {
+  logoName: string;
+  gitHubAccountLink: string;
+  websiteVersion: string;
+}) => {
+  const [active, setActive] = useState("");
+
+  console.log(active);
+
+  return (
+    <header className="px-5 py-4 flex items-center justify-between border-b-[1px] border-solid border-b-gray-100/10">
+      <div className="flex items-center gap-3.5">
+        <ReactScrollLink
+          to="#hero"
+          smooth
+          spy
+          onClick={() => setActive("#hero")}
+          className="cursor-pointer group"
+        >
+          <div className="flex items-center gap-2">
+            <div className="p-2.5 border-[1px] border-solid border-gray-100/25 rounded-full">
+              <FaUser className="scale-110 group-hover:text-primary transition-colors duration-200 ease-linear" />
+            </div>
+            <div className="text-xl font-medium lowercase">
+              {logoName || "Logo Name"}
+            </div>
+          </div>
+        </ReactScrollLink>
+
+        <div className="px-2 py-1 text-xs font-semibold text-gray-50/80 tracking-wide bg-gray-50/10 rounded-full">
+          <span>v</span>
+          {websiteVersion || "Website Version"}
+        </div>
+      </div>
+
+      <nav>
+        <ul className="flex items-center gap-5">
+          {navLinks.map((item) => (
+            <li key={item.url}>
+              <ReactScrollLink
+                to={item.url}
+                smooth
+                spy
+                onClick={() => setActive(item.url)}
+                className={`flex items-center gap-2 cursor-pointer hover:text-gray-50 transition-colors duration-200 ease-linear ${
+                  active === item.url ? "text-primary" : "text-gray-50/50"
+                }`}
+              >
+                <div>{item.icon}</div>
+                <div>{item.title}</div>
+              </ReactScrollLink>
+            </li>
+          ))}
+
+          <li>
+            <Link
+              href={gitHubAccountLink || "/"}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaGithub className="scale-125" />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
