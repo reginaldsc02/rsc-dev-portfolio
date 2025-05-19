50faec43450aaf980b2b1f1c5ae54ace93034c8f
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-19 17:03:49 +1200

    Create: Primary Button

---
+++ components/ui/Buttons.tsx
@@ -0,0 +1,16 @@
+import { BiError } from "react-icons/bi";
+
+export const PrimaryButton = ({
+  buttonText,
+  buttonIcon,
+}: {
+  buttonText: string;
+  buttonIcon: React.ReactNode;
+}) => {
+  return (
+    <button className="min-w-[200px] min-h-[60px] p-5 flex items-center justify-center gap-1.5 font-medium text-xl lowercase bg-primary rounded-full hover:bg-gray-50 hover:text-gray-950 transition-all duration-200 ease-linear cursor-pointer">
+      <span className="animate-pulse">{buttonIcon || <BiError />}</span>
+      {buttonText || "Button Text"}
+    </button>
+  );
+};
