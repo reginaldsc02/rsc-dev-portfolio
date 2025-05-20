c1551260ea1e89af301c43c646714be7ec0fd017
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-21 08:27:46 +1200

    Setup: Layout for Desktop

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,3 +1,26 @@
+import Link from "next/link";
+
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="px-5 py-4 border-t-[1px] border-solid border-t-gray-100/10">
+      <div className="p-5 flex items-center justify-between border-[1px] border-solid border-gray-100/10 rounded-lg">
+        <p>
+          &copy;{" "}
+          <Link
+            href={"https://github.com/reginaldsc02"}
+            target="_blank"
+            rel="noreferrer"
+            className="italic hover:text-primary transition-colors duration-200 ease-linear"
+          >
+            Reginald Sahil Chand
+          </Link>{" "}
+          - 2025, All Rights Reserved.
+        </p>
+
+        <p className="text-gray-50/50">
+          Designed & Developed with ❤️ | Powered by Next.js & TailwindCSS
+        </p>
+      </div>
+    </footer>
+  );
 };
