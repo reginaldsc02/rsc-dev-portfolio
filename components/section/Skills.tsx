7de8ace974f7d7be75c879491400140b8f19d668
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-21 07:21:46 +1200

    Setup: Skills Section - Desktop Layout

--- components/section/Skills.tsx
+++ components/section/Skills.tsx
@@ -1,3 +1,55 @@
+import { FaReact } from "react-icons/fa";
+import { knownSkills } from "../data/knownSkills";
+
 export const Skills = () => {
-  return <section id="#skills">Skills</section>;
+  return (
+    <section
+      id="skills"
+      className="my-5 p-2.5 border-y-[1px] border-solid border-y-gray-50/10"
+    >
+      <div className="w-full flex items-start justify-center flex-col p-2.5 border-y-[1px] border-solid border-y-gray-50/10">
+        <div className="flex items-center justify-center gap-2.5">
+          <div className="flex items-center gap-1.5 text-balance capitalize text-4xl font-bold">
+            <span className="animate-spin">
+              <FaReact />
+            </span>
+
+            <span className="font-medium">The Arsenal of Alchemy</span>
+
+            <span className="animate-spin">
+              <FaReact />
+            </span>
+          </div>
+
+          <p className="italic tracking-wide text-gray-50/50">
+            Crafting Magic with Code & Craft
+          </p>
+        </div>
+
+        <p className="mt-5">
+          Behind every flawless system is a toolbox wielded with mastery and
+          heart. From the quiet hum of backend servers to the rhythmic pulse of
+          APIs, my skills are the sparks that ignite innovation and keep the
+          engine roaring - precise, polished, and powerful. These are the
+          languages, tools, and frameworks I bend to my will, crafting solutions
+          that don’t just work, but resonate. Ready to dive in? Here’s what’s in
+          my arsenal.
+        </p>
+      </div>
+
+      <div className="mt-5 p-2.5 grid border-[1px] border-solid border-gray-50/10 rounded-lg">
+        <ul className="grid grid-cols-4 gap-2.5">
+          {knownSkills.map((skill) => (
+            <li
+              className={`w-full p-5 flex items-center gap-2 border-[1px] border-solid border-gray-50/10 rounded-lg hover:border-gray-50 transition-all duration-200 ease-linear`}
+              key={skill.id}
+            >
+              <span className="scale-125">{skill.icon}</span>
+              <span className="capitalize">{skill.title}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 };
