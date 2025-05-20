0ab14862b8d7f898e3333107403912b2ff44f82b
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-21 06:58:38 +1200

    Create: Skills array - object list

---
+++ components/data/knownSkills.tsx
@@ -0,0 +1,173 @@
+import { PiMicrosoftWordLogo, PiWindowsLogo } from "react-icons/pi";
+import {
+  RiCodeFill,
+  RiCss3Fill,
+  RiGithubFill,
+  RiHtml5Fill,
+  RiJavascriptFill,
+  RiNextjsFill,
+  RiNodejsFill,
+  RiPencilFill,
+  RiReactjsFill,
+  RiTailwindCssFill,
+  RiUbuntuFill,
+} from "react-icons/ri";
+import {
+  SiArchlinux,
+  SiCanva,
+  SiCplusplus,
+  SiExpress,
+  SiFedora,
+  SiFigma,
+  SiLinux,
+  SiMongodb,
+  SiMongoose,
+  SiStyledcomponents,
+  SiTypescript,
+} from "react-icons/si";
+
+export const knownSkills = [
+  {
+    title: "HTML 5",
+    icon: <RiHtml5Fill />,
+    id: "@html5",
+  },
+
+  {
+    title: "CSS 3",
+    icon: <RiCss3Fill />,
+    id: "@css3",
+  },
+
+  {
+    title: "JavaScript",
+    icon: <RiJavascriptFill />,
+    id: "@javascript",
+  },
+
+  {
+    title: "TypeScript",
+    icon: <SiTypescript />,
+    id: "@typescript",
+  },
+
+  {
+    title: "React.js",
+    icon: <RiReactjsFill />,
+    id: "@reactjs",
+  },
+
+  {
+    title: "Next.js",
+    icon: <RiNextjsFill />,
+    id: "@nextjs",
+  },
+
+  {
+    title: "TailwindCSS",
+    icon: <RiTailwindCssFill />,
+    id: "@tailwindcss",
+  },
+
+  {
+    title: "Styled Components",
+    icon: <SiStyledcomponents />,
+    id: "@styledcomponents",
+  },
+
+  {
+    title: "Node.js",
+    icon: <RiNodejsFill />,
+    id: "@nodejs",
+  },
+
+  {
+    title: "Express.js",
+    icon: <SiExpress />,
+    id: "@express",
+  },
+
+  {
+    title: "Mongoose ODM",
+    icon: <SiMongoose />,
+    id: "@mongooseodm",
+  },
+
+  {
+    title: "MongoDB",
+    icon: <SiMongodb />,
+    id: "@mongodb",
+  },
+
+  {
+    title: "C++",
+    icon: <SiCplusplus />,
+    id: "@cpp",
+  },
+
+  {
+    title: "Git & GitHub",
+    icon: <RiGithubFill />,
+    id: "@gitgithub",
+  },
+
+  {
+    title: "Visual Studio Code",
+    icon: <RiCodeFill />,
+    id: "@vscode",
+  },
+
+  {
+    title: "Windows Operating System",
+    icon: <PiWindowsLogo />,
+    id: "@mswindowsos",
+  },
+
+  {
+    title: "Linux",
+    icon: <SiLinux />,
+    id: "@linux",
+  },
+
+  {
+    title: "Ubuntu",
+    icon: <RiUbuntuFill />,
+    id: "@ubuntu",
+  },
+
+  {
+    title: "Fedora",
+    icon: <SiFedora />,
+    id: "@fedora",
+  },
+
+  {
+    title: "Arch Linux",
+    icon: <SiArchlinux />,
+    id: "@archlinux",
+  },
+
+  {
+    title: "UI & UX Design",
+    icon: <RiPencilFill />,
+    id: "@uiux",
+  },
+
+  {
+    title: "Figma",
+    icon: <SiFigma />,
+    id: "@figma",
+  },
+
+  {
+    title: "Canva",
+    icon: <SiCanva />,
+    id: "@canva",
+  },
+
+  {
+    title: "Microsoft Office 365",
+    icon: <PiMicrosoftWordLogo />,
+    id: "@msoffice365",
+  },
+];
