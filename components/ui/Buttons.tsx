51f6fb24b866f7d71686896ac157633777168ad4
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-21 09:55:19 +1200

    Add: Active style

--- components/ui/Buttons.tsx
+++ components/ui/Buttons.tsx
@@ -8,7 +8,7 @@ export const PrimaryButton = ({
   buttonIcon: React.ReactNode;
 }) => {
   return (
-    <button className="min-w-[200px] min-h-[60px] p-5 flex items-center justify-center gap-1.5 font-medium text-xl lowercase bg-primary rounded-full hover:bg-gray-50 hover:text-gray-950 transition-all duration-200 ease-linear cursor-pointer">
+    <button className="min-w-[200px] min-h-[60px] p-5 flex items-center justify-center gap-1.5 font-medium text-xl lowercase bg-primary rounded-full hover:bg-gray-50 hover:text-gray-950 transition-all duration-200 ease-linear cursor-pointer active:scale-95">
       <span className="animate-pulse">{buttonIcon || <BiError />}</span>
       {buttonText || "Button Text"}
     </button>
