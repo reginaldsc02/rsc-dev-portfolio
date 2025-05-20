e055c31129a106c31122ba031cb64c93fd215572
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-21 09:03:04 +1200

    Setup: Background Image

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <>
       <Toaster position="top-center" reverseOrder={false} />
+      <div className="w-full h-screen fixed bg-[url('/images/backgrounds/ai-gen-background-body.jpg')] bg-top bg-cover bg-no-repeat -z-10 opacity-5 blur-xl"></div>
       <MainLayout />
     </>
   );
