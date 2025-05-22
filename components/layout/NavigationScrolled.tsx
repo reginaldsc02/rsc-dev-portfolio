87ae8915ec0f29324936ff9c240571178647e1ca
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-22 14:00:25 +1200

    Remove: Logic to hide/unhide Floating Scrollbar

--- components/layout/NavigationScrolled.tsx
+++ components/layout/NavigationScrolled.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useEffect, useRef } from "react";
+import { Dispatch, SetStateAction, useRef } from "react";
 import { Navigation } from "./Navigation";
 
 export const NavigationScrolled = ({
@@ -10,30 +10,8 @@ export const NavigationScrolled = ({
   setActive: Dispatch<SetStateAction<string>>;
   gitHubAccountLink: string;
 }) => {
-  const navigationContainer = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const threshold = 50;
-
-    const handleScroll = () => {
-      if (!navigationContainer.current) return;
-
-      navigationContainer.current.style.visibility =
-        window.scrollY > threshold ? "visible" : "hidden";
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
-    <div
-      ref={navigationContainer}
-      className="w-fit mx-auto p-5 invisible fixed bottom-5 left-0 right-0 border-2 border-solid border-gray-50/10 bg-gray-950 rounded-lg z-50"
-    >
+    <div className="w-fit mx-auto p-5 invisible fixed bottom-5 left-0 right-0 border-2 border-solid border-gray-50/10 bg-gray-950 rounded-lg z-50">
       <Navigation
         tag="div"
         ariaLabel="Secondary Navigation"
