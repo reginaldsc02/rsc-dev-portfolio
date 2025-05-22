e68fb0fef8cef04102d7c91b0ab97e7f2dc07536
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-22 14:26:43 +1200

    Update, Refactor, Scaled Up::: Add: IntersectionObserver Logic to hide/unhide Floating Navigation.

--- components/layout/MainLayout.tsx
+++ components/layout/MainLayout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { About } from "../section/About";
 import { Contact } from "../section/Contact";
 import { Hero } from "../section/Hero";
@@ -12,9 +12,39 @@ import { NavigationScrolled } from "./NavigationScrolled";
 
 export const MainLayout = () => {
   const [active, setActive] = useState("");
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
+  const navigationScrolledHelperRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (!entry.isIntersecting) {
+          setIsIntersecting(true);
+        } else {
+          setIsIntersecting(false);
+        }
+      },
+
+      {
+        root: null,
+        threshold: 0.01,
+      }
+    );
+
+    if (navigationScrolledHelperRef.current) {
+      observer.observe(navigationScrolledHelperRef.current);
+    }
+  }, []);
 
   return (
     <div className="min-w-full min-h-screen grid grid-cols-1 grid-rows-[auto_1fr_auto]">
+      <div
+        ref={navigationScrolledHelperRef}
+        id="navigation-scrolled-helper"
+        className="w-full h-16 absolute top-0 left-0 pointer-events-none -z-50"
+      ></div>
+
       <Header
         active={active}
         setActive={setActive}
@@ -24,6 +54,7 @@ export const MainLayout = () => {
       />
 
       <NavigationScrolled
+        isIntersecting={isIntersecting}
         active={active}
         setActive={setActive}
         gitHubAccountLink="https://github.com/reginaldsc02"
--- components/layout/NavigationScrolled.tsx
+++ components/layout/NavigationScrolled.tsx
@@ -1,17 +1,25 @@
-import { Dispatch, SetStateAction, useRef } from "react";
+import { Dispatch, SetStateAction } from "react";
 import { Navigation } from "./Navigation";
 
 export const NavigationScrolled = ({
+  isIntersecting,
   active,
   setActive,
   gitHubAccountLink,
 }: {
+  isIntersecting: boolean;
   active: string;
   setActive: Dispatch<SetStateAction<string>>;
   gitHubAccountLink: string;
 }) => {
   return (
-    <div className="w-fit mx-auto p-5 invisible fixed bottom-5 left-0 right-0 border-2 border-solid border-gray-50/10 bg-gray-950 rounded-lg z-50">
+    <div
+      className={`w-fit mx-auto p-5 ${
+        isIntersecting
+          ? "opacity-100 pointer-events-auto"
+          : "opacity-0 pointer-events-none"
+      } fixed bottom-5 left-0 right-0 border-2 border-solid border-gray-50/10 bg-gray-950 rounded-lg z-50 transition-opacity duration-200 ease-linear`}
+    >
       <Navigation
         tag="div"
         ariaLabel="Secondary Navigation"
