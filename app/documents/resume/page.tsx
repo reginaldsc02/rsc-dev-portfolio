d6b4b2d726e00e0d973d27e2436a36b9d3642857
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-24 17:19:29 +1200

    Update: Border Style

--- app/documents/resume/page.tsx
+++ app/documents/resume/page.tsx
@@ -6,7 +6,7 @@ export default function ResumePreview() {
     <main className="w-full p-1.5 min-h-screen">
       <Link
         href={"/#about"}
-        className="w-full mb-1.5 p-5 flex items-center gap-2.5 text-gray-50/50 hover:text-gray-50 transition-colors duration-200 ease-linear group border-[1px] border-solid border-gray-50/10 rounded-lg"
+        className="w-full mb-1.5 p-5 flex items-center gap-2.5 text-gray-50/50 hover:text-gray-50 transition-colors duration-200 ease-linear group border-full-default rounded-lg"
       >
         <span>
           <FaArrowLeft />
