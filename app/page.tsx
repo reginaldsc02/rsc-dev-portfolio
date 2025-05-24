e077bcd9ff16547005d99dbd0d9182d6612dafcb
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-24 16:47:26 +1200

    Update: Code spacing and positioning for better readability

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ import { Toaster } from "react-hot-toast";
 export default function Home() {
   return (
     <>
-      <Toaster position="top-center" reverseOrder={false} />
-      <CustomCursor />
       <div className="w-full h-screen fixed bg-[url('/images/backgrounds/ai-gen-background-body.jpg')] bg-top bg-cover bg-no-repeat -z-10 opacity-5 blur-xl"></div>
 
+      <Toaster position="top-center" reverseOrder={false} />
+      <CustomCursor />
       <MainLayout />
     </>
   );
