f32f6fb0b77665a4b01b96988ccab3b2b1bc2f83
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-24 22:39:23 +1200

    feat: add manual loading state with Suspense fallback for better UX

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
+"use client";
+
 import { MainLayout } from "@/components/layout/MainLayout";
 import { CustomCursor } from "@/components/ui/CustomCursor";
 import { Loader } from "@/components/ui/Loader";
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { Toaster } from "react-hot-toast";
 
 export default function Home() {
-  return (
+  const [isLoading, setIsLoading] = useState(true);
+  const timeoutIsTwoSeconds = 2000;
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, timeoutIsTwoSeconds);
+  }, []);
+
+  return isLoading ? (
+    <Loader />
+  ) : (
     <Suspense fallback={<Loader />}>
       <div className="w-full h-screen fixed bg-[url('/images/backgrounds/ai-gen-background-body.jpg')] bg-top bg-cover bg-no-repeat -z-10 opacity-5 blur-xl"></div>
 
