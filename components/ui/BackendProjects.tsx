7ae6bf217a68cd05caab00b564e86fdc7919ab64
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-26 21:34:44 +1200

    refactor: remove background image and extra styles from project header section for cleaner and focused layout

--- components/ui/BackendProjects.tsx
+++ components/ui/BackendProjects.tsx
@@ -8,30 +8,23 @@ import { SectionParagraph } from "./SectionParagraph";
 export const BackendProjects = () => {
   return (
     <>
-      <div className="my-5 flex justify-between gap-10 text-balance border-full-default rounded-lg">
-        <div className="p-5">
-          <SectionHeader
-            icon={<FaBriefcase />}
-            title="Engine Room Highlights"
-            subtitle="Featured Projects"
-          />
+      <div className="p-5 border-full-default rounded-lg">
+        <SectionHeader
+          icon={<FaBriefcase />}
+          title="Engine Room Highlights"
+          subtitle="Featured Projects"
+        />
 
-          <SectionParagraph>
-            <Paragraph
-              paragraph="A collection of robust backend microservices and management systems
+        <SectionParagraph>
+          <Paragraph
+            paragraph="A collection of robust backend microservices and management systems
             that power smooth, scalable, and efficient digital experiences
             behind the scenes. From notification management to file uploads and
             content handling, these projects showcase my knack for building
             solid foundations that keep everything running like clockwork - no
             drama, just dependable code doing its thing."
-            />
-          </SectionParagraph>
-        </div>
-
-        <div
-          role="img"
-          className="hidden xl:block w-[1300px] min-h-40 bg-[url('/images/projects/ai-gen-background-projects.png')] bg-center bg-cover bg-no-repeat rounded-tr-lg rounded-br-lg filter saturate-200"
-        ></div>
+          />
+        </SectionParagraph>
       </div>
 
       <div className="grid xl:grid-cols-3 gap-2.5">
