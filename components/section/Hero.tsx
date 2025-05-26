f2d50e28dfb68e9176717f9dedad57692dea7ea3
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-27 01:44:48 +1200

    feat: added BorderControl & PaddingInner to Hero section and tweaked styles

--- components/section/Hero.tsx
+++ components/section/Hero.tsx
@@ -3,6 +3,8 @@ import { Button } from "../ui/Button";
 import { Link } from "react-scroll";
 import { Dispatch, SetStateAction } from "react";
 import { Paragraph } from "../ui/Paragraph";
+import { PaddingInner } from "../ui/PaddingInner";
+import { BorderControl } from "../ui/BorderControl";
 
 export const Hero = ({
   setActive,
@@ -12,68 +14,84 @@ export const Hero = ({
   return (
     <section id="hero">
       <div className="pt-14 grid gap-2.5">
-        <div className="p-5 py-2 flex md:items-center justify-between flex-col md:flex-row text-gray-50/50 tracking-tighter border-y-default">
-          <div className="lowercase">
-            Meet Reginald Chand - full-stack developer, UX Enthusiast and
-            experience architect.
-          </div>
+        <BorderControl borderY>
+          <div className="p-5 py-2 flex md:items-center justify-between flex-col md:flex-row text-gray-50/50 tracking-tighter">
+            <div className="lowercase">
+              Meet Reginald Chand - full-stack developer, UX Enthusiast and
+              experience architect.
+            </div>
 
-          <div className="lowercase font-medium text-sm flex items-center gap-1.5">
-            <div className="text-gray-50 md:text-gray-50/50">#Open to work</div>
+            <div className="lowercase font-medium text-sm flex items-center gap-1.5">
+              <div className="text-gray-50 md:text-gray-50/50">
+                #Open to work
+              </div>
 
-            <div
-              role="img"
-              aria-label="Open to work indicator"
-              title="Open to work indicator"
-              className="w-4 h-4 flex items-center justify-center border-[1px] border-solid border-gray-50 rounded-full animate-pulse"
-            >
-              <div className="w-2 h-2 bg-white rounded-full animate-ping"></div>
+              <div
+                role="img"
+                aria-label="Open to work indicator"
+                title="Open to work indicator"
+                className="w-4 h-4 flex items-center justify-center border-[1px] border-solid border-gray-50 rounded-full animate-pulse"
+              >
+                <div className="w-2 h-2 bg-white rounded-full animate-ping"></div>
+              </div>
             </div>
           </div>
-        </div>
+        </BorderControl>
 
-        <h1 className="p-5 text-3xl md:text-5xl text-balance font-bold text-gray-50/50 tracking-tighter leading-6 md:leading-10 uppercase border-y-default">
-          <span className="text-gray-50">I build experiences</span> where
-          thoughtful code meets elegant design and real human need.
+        <h1 className="text-3xl md:text-5xl text-balance font-bold text-gray-50/50 tracking-tighter leading-6 md:leading-10 uppercase">
+          <BorderControl borderY>
+            <PaddingInner>
+              <span className="text-gray-50">I build experiences</span> where
+              thoughtful code meets elegant design and real human need.
+            </PaddingInner>
+          </BorderControl>
         </h1>
 
-        <div className="p-5 lowercase text-balance border-y-default">
-          <Paragraph
-            paragraph=" With a passion for elegant code and user-first design, I engineer
+        <div className="lowercase text-balance">
+          <BorderControl borderY>
+            <PaddingInner>
+              <Paragraph
+                paragraph=" With a passion for elegant code and user-first design, I engineer
             full-stack solutions that are as scalable as they are seamless."
-          />
+              />
 
-          <Paragraph
-            paragraph="Every project I build is crafted with intention, blending clean
+              <Paragraph
+                paragraph="Every project I build is crafted with intention, blending clean
             architecture, intuitive interfaces, and a touch of creative soul."
-            isImportant
-          />
+                isImportant
+              />
 
-          <Paragraph
-            paragraph=" Whether it's backend logic or frontend finesse, I deliver
+              <Paragraph
+                paragraph=" Whether it's backend logic or frontend finesse, I deliver
             software that feels as good as it functions."
-          />
+              />
+            </PaddingInner>
+          </BorderControl>
         </div>
 
-        <div className="my-5 border-y-default">
-          <div className="w-fit">
-            <Link
-              to="projects"
-              smooth
-              spy
-              onClick={() => setActive("projects")}
-            >
-              <Button buttonIcon={<FaSearch />} buttonText="View Projects" />
-            </Link>
-          </div>
+        <div className="my-5">
+          <BorderControl borderY>
+            <div className="w-fit">
+              <Link
+                to="projects"
+                smooth
+                spy
+                onClick={() => setActive("projects")}
+              >
+                <Button buttonIcon={<FaSearch />} buttonText="View Projects" />
+              </Link>
+            </div>
+          </BorderControl>
         </div>
 
-        <blockquote className="flex items-center justify-end pt-10 px-5 text-right text-gray-50/50 border-b-[1px] border-dashed border-b-gray-50/10">
-          <p className="max-w-[500px]">
-            “In every line of code, a story whispers - where logic dances with,
-            dreams, and software becomes soul.” - <b>ChatGPT</b> 💫
-          </p>
-        </blockquote>
+        <BorderControl borderY>
+          <blockquote className="flex justify-end pt-14 px-5 text-right text-gray-50/50">
+            <p className="max-w-[500px]">
+              “In every line of code, a story whispers - where logic dances
+              with, dreams, and software becomes soul.” - <b>ChatGPT</b> 💫
+            </p>
+          </blockquote>
+        </BorderControl>
       </div>
     </section>
   );
