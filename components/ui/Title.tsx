b26fdaad5f322cb96659fda7b529151be78860d4
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-26 18:38:07 +1200

    remove: default margin-bottom

--- components/ui/Title.tsx
+++ components/ui/Title.tsx
@@ -3,7 +3,7 @@ import { BiError } from "react-icons/bi";
 
 export const Title = ({ icon, title }: { icon: ReactNode; title: string }) => {
   return (
-    <div className="mb-2.5 flex items-center gap-1.5 text-balance capitalize text-4xl font-bold">
+    <div className="flex items-center gap-1.5 text-balance capitalize text-4xl font-bold">
       <div className="hidden md:block">{icon || <BiError />}</div>
       <h2 className="font-medium">{title || "Title"}</h2>
     </div>
