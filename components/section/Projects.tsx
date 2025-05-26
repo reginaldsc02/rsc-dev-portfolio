451cbc8e050c3b57dab4cbbc7d652c2db77634a3
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-26 21:55:41 +1200

    style: update border styles to use custom utility CSS classes for consistency

--- components/section/Projects.tsx
+++ components/section/Projects.tsx
@@ -2,10 +2,7 @@ import { BackendProjects } from "../ui/BackendProjects";
 
 export const Projects = () => {
   return (
-    <section
-      id="projects"
-      className="mt-16 p-2.5 border-t-[1px] border-solid border-t-gray-50/10"
-    >
+    <section id="projects" className="mt-16 p-2.5 border-t-default">
       <BackendProjects />
     </section>
   );
