c95281dc345393419997e8f68e13d560f7b90548
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-27 02:00:31 +1200

    feat: update footer styles; add BorderControl, PaddingInner, and PaddingOuter components

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -10,6 +10,10 @@ import {
 } from "react-icons/fa";
 import toast from "react-hot-toast";
 import { useGitHubApi } from "@/api/useGitHubApi";
+import { BorderControl } from "../ui/BorderControl";
+import { PaddingInner } from "../ui/PaddingInner";
+import { Paragraph } from "../ui/Paragraph";
+import { PaddingOuter } from "../ui/PaddingOuter";
 
 export const Footer = ({
   setActive,
@@ -25,95 +29,109 @@ export const Footer = ({
   }, []);
 
   return (
-    <footer className="px-2.5 md:px-5 py-4 border-t-default">
-      <div className="p-5 flex flex-col md:flex-row md:items-center justify-between gap-2.5 border-full-default rounded-lg">
-        <p>
-          &copy;{" "}
-          <Link
-            href={"https://github.com/reginaldsc02"}
-            target="_blank"
-            rel="noreferrer"
-            className="underline font-bold text-gray-50/50 hover:text-gray-50 transition-colors duration-200 ease-linear rounded-sm"
-          >
-            Reginald Sahil Chand
-          </Link>{" "}
-          - {currentDate}, All Rights Reserved.
-        </p>
+    <footer>
+      <BorderControl borderY>
+        <PaddingInner>
+          <div className="flex flex-col md:flex-row md:items-center justify-between gap-2.5">
+            <p>
+              &copy;{" "}
+              <Link
+                href={"https://github.com/reginaldsc02"}
+                target="_blank"
+                rel="noreferrer"
+                className="underline font-bold text-gray-50/50 hover:text-gray-50 transition-colors duration-200 ease-linear rounded-sm"
+              >
+                Reginald Sahil Chand
+              </Link>{" "}
+              - {currentDate}, All Rights Reserved.
+            </p>
 
-        <p className="text-gray-50/50">
-          Designed & Developed with <span className="text-gray-50">❤️</span> |
-          Powered by Next.js & TailwindCSS
-        </p>
-      </div>
+            <Paragraph
+              paragraph="Designed & Developed with ❤️
+            Powered by Next.js & TailwindCSS"
+            />
+          </div>
+        </PaddingInner>
 
-      <div className="w-full mt-5 py-5 flex flex-col md:flex-row md:items-center md:justify-between gap-5 border-y-default">
-        <div className="group">
-          <ReactScrollLink
-            to="hero"
-            smooth
-            spy
-            onClick={() => {
-              setActive("hero");
-              toast.success("Yeeted to the top ✨");
-            }}
-            className="flex items-center gap-1.5 cursor-pointer text-xs font-bold uppercase"
-          >
-            <FaArrowUp className="group-hover:animate-bounce" />
-            <div className="group-active:scale-95">Scroll to top</div>
-          </ReactScrollLink>
-        </div>
+        <BorderControl borderY>
+          <PaddingOuter>
+            <BorderControl borderFull isRounded>
+              <PaddingInner>
+                <div className="w-full flex flex-col md:flex-row md:items-center md:justify-between gap-5">
+                  <div className="group">
+                    <ReactScrollLink
+                      to="hero"
+                      smooth
+                      spy
+                      onClick={() => {
+                        setActive("hero");
+                        toast.success("Yeeted to the top ✨");
+                      }}
+                      className="flex items-center gap-1.5 cursor-pointer text-xs font-bold uppercase"
+                    >
+                      <FaArrowUp className="group-hover:animate-bounce" />
+                      <div className="group-active:scale-95">Scroll to top</div>
+                    </ReactScrollLink>
+                  </div>
 
-        <div className="mt-2 md:mt-0 grid grid-cols-2 md:grid-cols-3 gap-5 text-gray-50/50">
-          <Link
-            href={"https://www.linkedin.com/in/reginaldsc02/"}
-            target="_blank"
-            rel="noreferrer"
-            className="flex items-center gap-2 hover:text-gray-50 transition-colors duration-200 ease-linear rounded-md"
-          >
-            <FaLinkedin />
-            <div className="active:scale-95">LinkedIn</div>
-          </Link>
+                  <div className="mt-2 md:mt-0 grid grid-cols-2 md:grid-cols-3 gap-5 text-gray-50/50">
+                    <Link
+                      href={"https://www.linkedin.com/in/reginaldsc02/"}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="flex items-center gap-2 hover:text-gray-50 transition-colors duration-200 ease-linear rounded-md"
+                    >
+                      <FaLinkedin />
+                      <div className="active:scale-95">LinkedIn</div>
+                    </Link>
 
-          <Link
-            href={"https://github.com/reginaldsc02"}
-            target="_blank"
-            rel="noreferrer"
-            className="flex items-center gap-2 hover:text-gray-50 transition-colors duration-200 ease-linear rounded-md"
-          >
-            <FaGithub />
-            <div className="active:scale-95">GitHub</div>
-          </Link>
+                    <Link
+                      href={"https://github.com/reginaldsc02"}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="flex items-center gap-2 hover:text-gray-50 transition-colors duration-200 ease-linear rounded-md"
+                    >
+                      <FaGithub />
+                      <div className="active:scale-95">GitHub</div>
+                    </Link>
 
-          <Link
-            href={"https://www.facebook.com/reginaldsc02/"}
-            target="_blank"
-            rel="noreferrer"
-            className="flex items-center gap-2 hover:text-gray-50 transition-colors duration-200 ease-linear rounded-md"
-          >
-            <FaFacebook />
-            <div className="active:scale-95">Facebook</div>
-          </Link>
-        </div>
-      </div>
+                    <Link
+                      href={"https://www.facebook.com/reginaldsc02/"}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="flex items-center gap-2 hover:text-gray-50 transition-colors duration-200 ease-linear rounded-md"
+                    >
+                      <FaFacebook />
+                      <div className="active:scale-95">Facebook</div>
+                    </Link>
+                  </div>
+                </div>
+              </PaddingInner>
+            </BorderControl>
+          </PaddingOuter>
+        </BorderControl>
 
-      <div className="mt-5 pt-5 lowercase text-balance border-t-default">
-        <div className="flex items-center gap-2 text-sm text-gray-50/50">
-          This website was last updated on:{" "}
-          {response.data.updated_at
-            ? new Date(response?.data?.updated_at).toLocaleString()
-            : "Information currently unavailable"}
-        </div>
+        <PaddingOuter>
+          <div className="lowercase text-balance">
+            <div className="flex items-center gap-2 text-sm text-gray-50/50">
+              This website was last updated on:{" "}
+              {response.data.updated_at
+                ? new Date(response?.data?.updated_at).toLocaleString()
+                : "Information currently unavailable"}
+            </div>
 
-        <Link
-          href={"https://github.com/reginaldsc02/rsc-dev-portfolio"}
-          target="_blank"
-          rel="noreferrer"
-          className="w-fit flex items-center gap-2 lowercase group"
-        >
-          <FaCode className="group-hover:animate-pulse" />
-          <div className="group-active:scale-95"> View source code</div>
-        </Link>
-      </div>
+            <Link
+              href={"https://github.com/reginaldsc02/rsc-dev-portfolio"}
+              target="_blank"
+              rel="noreferrer"
+              className="w-fit flex items-center gap-2 lowercase group"
+            >
+              <FaCode className="group-hover:animate-pulse" />
+              <div className="group-active:scale-95"> View source code</div>
+            </Link>
+          </div>
+        </PaddingOuter>
+      </BorderControl>
     </footer>
   );
 };
