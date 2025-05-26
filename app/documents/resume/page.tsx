aeab987849f04e115a96c1c98f1e3f4e168db903
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-27 06:01:25 +1200

    refactor: ResumePreview UI

--- app/documents/resume/page.tsx
+++ app/documents/resume/page.tsx
@@ -1,23 +1,27 @@
+import { LabeledIcon } from "@/components/ui/LabeledIcon";
+import { PaddingInner } from "@/components/ui/PaddingInner";
+import { PaddingOuter } from "@/components/ui/PaddingOuter";
 import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa";
 
 export default function ResumePreview() {
   return (
-    <main className="w-full p-1.5 min-h-screen">
-      <Link
-        href={"/"}
-        className="w-full mb-1.5 p-5 flex items-center gap-2.5 text-gray-50/50 hover:text-gray-50 transition-colors duration-200 ease-linear group border-full-default rounded-lg"
-      >
-        <span>
-          <FaArrowLeft />
-        </span>
-        <span className="group-active:scale-95">Back to home</span>
-      </Link>
+    <main className="w-full min-h-screen">
+      <PaddingOuter>
+        <Link
+          href={"/"}
+          className="w-full mb-2.5 block text-gray-50/50 hover:text-gray-50 transition-colors duration-200 ease-linear group border-full-default rounded-lg"
+        >
+          <PaddingInner>
+            <LabeledIcon icon={<FaArrowLeft />} label="Back to home" />
+          </PaddingInner>
+        </Link>
 
-      <iframe
-        src="/documents/resume-reginald-chand.pdf"
-        className="w-full h-screen rounded-lg"
-      ></iframe>
+        <iframe
+          src="/documents/resume-reginald-chand.pdf"
+          className="w-full h-screen rounded-lg"
+        ></iframe>
+      </PaddingOuter>
     </main>
   );
 }
