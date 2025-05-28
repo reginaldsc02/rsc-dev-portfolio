f8ce67a5a0f4a8a2446cd06b662b1c0a12675a20
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-29 02:54:03 +1200

    create: frontendProjectsList

---
+++ data/frontendProjectsList.tsx
@@ -0,0 +1,16 @@
+import { BiLogoTypescript } from "react-icons/bi";
+
+export const frontendProjectsList = [
+  {
+    imageSrc: "/images/projects/ai-gen-mppw.png",
+    imageAltText: "Personal Portfolio Website",
+    title: "Personal Portfolio Website",
+    link: "https://github.com/reginaldsc02/rsc-dev-portfolio",
+    repoName: "rsc-dev-portfolio",
+    description:
+      "This repository contains my personal portfolio, highlighting my expertise in full stack development with React, Next.js, Node.js, TypeScript, and Tailwind CSS.",
+    programmingLanguage: "TypeScript",
+    programmingLanguageIcon: <BiLogoTypescript />,
+    id: "@ppw",
+  },
+];
