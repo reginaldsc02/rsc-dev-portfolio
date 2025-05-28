628986747874e54bc266786f8c47afb08871f5fa
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-28 21:10:30 +1200

    feat: Add MainContainer Component. The central container for all pages.

--- components/layout/MainLayout.tsx
+++ components/layout/MainLayout.tsx
@@ -8,7 +8,7 @@ import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { NavigationScrolled } from "./NavigationScrolled";
 import { NavigationOnScrolledIntersectionObserver } from "../ui/NavigationOnScrolledIntersectionObserver";
-import { BorderControl } from "../ui/BorderControl";
+import { MainContainer } from "../ui/MainContainer";
 
 export const MainLayout = () => {
   const [active, setActive] = useState("");
@@ -44,17 +44,13 @@ export const MainLayout = () => {
         gitHubAccountLink="https://github.com/reginaldsc02"
       />
 
-      <main className="w-full 2xl:max-w-[1600px] mx-auto">
-        <div className="w-[calc(100%-1rem)] md:w-[calc(100%-5rem)] mx-auto min-h-screen">
-          <BorderControl borderX>
-            <Hero setActive={setActive} />
-            <Projects />
-            <About />
-            <Skills />
-            <Contact />
-          </BorderControl>
-        </div>
-      </main>
+      <MainContainer>
+        <Hero setActive={setActive} />
+        <Projects />
+        <About />
+        <Skills />
+        <Contact />
+      </MainContainer>
 
       <Footer setActive={setActive} />
     </div>
