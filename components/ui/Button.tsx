80df1ca5d82f10c2ac8fb0e9bab44e3b9c9db10d
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-29 02:21:22 +1200

    style: add style for, if button is disabled

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -19,7 +19,7 @@ export const Button = ({
   const secondaryButtonStyle =
     "text-gray-50/50 bg-transparent hover:border-gray-50 hover:text-gray-50 border-gray-50/50";
 
-  const buttonClass = `p-5 text-xl lowercase border-2 border-solid rounded-full transition-all duration-200 ease-linear cursor-pointer ${
+  const buttonClass = `p-5 text-xl lowercase border-2 border-solid rounded-full transition-all duration-200 ease-linear cursor-pointer disabled:pointer-events-none disabled:opacity-25 ${
     buttonIsPrimary ? primaryButtonStyle : secondaryButtonStyle
   }`;
 
