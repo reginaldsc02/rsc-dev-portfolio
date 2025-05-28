477f6334ca810d5b81523f359c7b12a3e835cb15
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-28 16:31:26 +1200

    setup(basic): All Projects Page *NO CONTENT AS OF NOW*

--- components/section/Projects.tsx
+++ components/section/Projects.tsx
@@ -1,6 +1,8 @@
+import Link from "next/link";
 import { BackendProjects } from "../ui/BackendProjects";
 import { BorderControl } from "../ui/BorderControl";
 import { PaddingOuter } from "../ui/PaddingOuter";
+import { PaddingInner } from "../ui/PaddingInner";
 
 export const Projects = () => {
   return (
@@ -8,6 +10,17 @@ export const Projects = () => {
       <BorderControl borderY>
         <PaddingOuter>
           <BackendProjects />
+
+          <div className="mt-2.5">
+            <BorderControl borderFull isRounded>
+              <Link
+                href={"projects/all"}
+                className="flex items-center justify-center lowercase text-gray-50/50 hover:text-gray-50 transition-colors duration-200 ease-linear"
+              >
+                <PaddingInner>View all projects</PaddingInner>
+              </Link>
+            </BorderControl>
+          </div>
         </PaddingOuter>
       </BorderControl>
     </section>
