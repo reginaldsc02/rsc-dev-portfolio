dc43a625361bd1a360b4e5a61445f10b83af6a91
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-31 05:09:07 +1200

    style: add padding top

--- components/ui/MainContainer.tsx
+++ components/ui/MainContainer.tsx
@@ -3,7 +3,7 @@ export const MainContainer = ({
 }: Readonly<{ children: React.ReactNode }>) => {
   return (
     <main className="w-full 2xl:max-w-[1600px] mx-auto mt-16">
-      <div className="w-[calc(100%-1rem)] md:w-[calc(100%-5rem)] mx-auto border-x-default">
+      <div className="w-[calc(100%-1rem)] md:w-[calc(100%-5rem)] mx-auto pt-spacing-default-10px border-x-default">
         {children}
       </div>
     </main>
