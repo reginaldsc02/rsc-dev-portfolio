e6d78abb776140e3ee810c63b9be301f39250a11
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-31 10:41:15 +1200

    style: add zindex

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -4,7 +4,7 @@ export const Header = ({
   children,
 }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <header className="w-full h-16 px-spacing-default-20px sticky top-0 left-0 flex items-center border-b-default bg-gray-950">
+    <header className="w-full h-16 px-spacing-default-20px sticky top-0 left-0 flex items-center border-b-default bg-gray-950 z-40">
       <div className="flex gap-spacing-default-10px mr-spacing-default-10px">
         <Link
           href="/"
