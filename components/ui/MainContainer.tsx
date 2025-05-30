8d14c8ae5befa811322706fe779323d312715e99
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-31 05:10:37 +1200

    style: remove padding & margin top

--- components/ui/MainContainer.tsx
+++ components/ui/MainContainer.tsx
@@ -2,8 +2,8 @@ export const MainContainer = ({
   children,
 }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <main className="w-full 2xl:max-w-[1600px] mx-auto mt-16">
-      <div className="w-[calc(100%-1rem)] md:w-[calc(100%-5rem)] mx-auto pt-spacing-default-10px border-x-default">
+    <main className="w-full 2xl:max-w-[1600px] mx-auto">
+      <div className="w-[calc(100%-1rem)] md:w-[calc(100%-5rem)] mx-auto border-x-default">
         {children}
       </div>
     </main>
