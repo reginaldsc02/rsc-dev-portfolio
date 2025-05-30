faa5b91251c65df47e048a0c53b1af893ae29ecd
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-31 09:56:42 +1200

    feat: add updated header component

---
+++ components/ui/Header.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export const Header = ({
+  children,
+}: Readonly<{ children: React.ReactNode }>) => {
+  return (
+    <header className="w-full h-16 px-spacing-default-20px sticky top-0 left-0 flex items-center border-b-default bg-gray-950">
+      <div className="flex gap-spacing-default-10px mr-spacing-default-10px">
+        <Link
+          href="/"
+          className="text-default-opacity-50 transition-colors hover:text-primary"
+        >
+          @reginaldchand
+        </Link>
+        |
+      </div>
+
+      <div className="w-full flex items-center justify-between">{children}</div>
+    </header>
+  );
+};
