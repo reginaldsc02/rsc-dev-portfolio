3ef7924bafeb777e80f66fbfd177f33de8bcb87c
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-31 13:37:00 +1200

    update: font size

--- components/ui/Title.tsx
+++ components/ui/Title.tsx
@@ -1,6 +1,6 @@
 export const Title = ({ title }: { title: string }) => {
   return (
-    <h2 className="font-semibold text-balance capitalize text-2xl sm:text-3xl md:text-4xl">
+    <h2 className="font-bold text-balance capitalize text-3xl md:text-4xl">
       {title || "Title"}
     </h2>
   );
