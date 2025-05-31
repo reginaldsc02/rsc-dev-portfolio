68d575914bb903f1bd44feb16e6e24ac2639127b
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-05-31 14:27:48 +1200

    refactor: props

--- components/ui/LabeledIcon.tsx
+++ components/ui/LabeledIcon.tsx
@@ -3,21 +3,13 @@ import { BiError } from "react-icons/bi";
 export const LabeledIcon = ({
   icon,
   label,
-  animateIcon = false,
-  isLargeIcon = false,
 }: {
   icon: React.ReactNode;
   label: string;
-  animateIcon?: boolean;
-  isLargeIcon?: boolean;
 }) => {
-  const iconClass = `max-[375px]:hidden ${
-    animateIcon ? "animate-pulse" : "animate-none"
-  } ${isLargeIcon ? "text-2xl" : "text-inherit"}`;
-
   return (
     <div className="flex items-center gap-spacing-default-6px">
-      <div className={iconClass}>{icon || <BiError />}</div>
+      <div className="max-[375px]:hidden">{icon || <BiError />}</div>
       <div>{label || "Icon Label"}</div>
     </div>
   );
