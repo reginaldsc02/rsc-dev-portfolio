f4a8f7b65edb6eaa3e1b1f08898a04a51eaa63cf
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-06-02 00:02:04 +1200

    add: PrimaryHeader

--- components/layout/MainLayout.tsx
+++ components/layout/MainLayout.tsx
@@ -4,10 +4,13 @@ import { Hero } from "../section/Hero";
 import { Projects } from "../section/Projects";
 import { Skills } from "../section/Skills";
 import { MainContainer } from "../ui/MainContainer";
+import { PrimaryHeader } from "../ui/PrimaryHeader";
 
 export const MainLayout = () => {
   return (
     <div className="min-w-full min-h-screen grid grid-rows-[auto_1fr_auto] opacity-0 animate-fade-in">
+      <PrimaryHeader />
+
       <MainContainer>
         <Hero />
         <Projects />
