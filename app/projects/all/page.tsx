fbeb531fb722f990ccaadf6b8a69230f67045d5c
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-06-02 01:05:12 +1200

    add: Header component for projects

--- app/projects/all/page.tsx
+++ app/projects/all/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { Button } from "@/components/ui/Button";
+import { Header } from "@/components/ui/Header";
 import { MainContainer } from "@/components/ui/MainContainer";
 import { ProjectCard } from "@/components/ui/ProjectCard";
 import { ThreeColumnGrid } from "@/components/ui/ThreeColumnGrid";
 import { backendProjectsList } from "@/data/backendProjectsList";
 import { frontendProjectsList } from "@/data/frontendProjectsList";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function AllProjects() {
@@ -35,49 +37,69 @@ export default function AllProjects() {
       ?.project || [];
 
   return (
-    <MainContainer>
-      <div className="p-spacing-default-10px border-b-default">
-        <div className="gap-spacing-default-10px border-full-default rounded-default-8px">
-          <div className="p-spacing-default-10px flex items-center flex-wrap gap-spacing-default-10px">
-            <Button
-              text={"frontend"}
-              isPill
-              isPrimary={projectCategory !== projectCategories.frontendProjects}
-              onClick={() =>
-                setProjectCategory(projectCategories.frontendProjects)
-              }
-              disabled={projectCategory === projectCategories.frontendProjects}
-            />
-
-            <Button
-              text={"backend"}
-              isPill
-              isPrimary={projectCategory !== projectCategories.backendProjects}
-              onClick={() =>
-                setProjectCategory(projectCategories.backendProjects)
-              }
-              disabled={projectCategory === projectCategories.backendProjects}
-            />
-          </div>
+    <>
+      <Header>
+        <div className="w-full flex items-center justify-end">
+          <Link
+            href="/"
+            aria-label="Go to home page"
+            className="px-2 py-1 block cursor-pointer border-full-default rounded-full hover:border-primary! transition-all"
+          >
+            Home
+          </Link>
         </div>
-      </div>
+      </Header>
 
-      <div className="p-spacing-default-10px">
-        <ThreeColumnGrid>
-          {project.map((project) => (
-            <div key={project.id}>
-              <ProjectCard
-                imageSrc={project.imageSrc}
-                imageAltText={project.imageAltText}
-                title={project.title}
-                link={project.link}
-                repoName={project.repoName}
-                description={project.description}
+      <MainContainer>
+        <div className="p-spacing-default-10px border-b-default">
+          <div className="gap-spacing-default-10px border-full-default rounded-default-8px">
+            <div className="p-spacing-default-10px flex items-center flex-wrap gap-spacing-default-10px">
+              <Button
+                text={"frontend"}
+                isPill
+                isPrimary={
+                  projectCategory !== projectCategories.frontendProjects
+                }
+                onClick={() =>
+                  setProjectCategory(projectCategories.frontendProjects)
+                }
+                disabled={
+                  projectCategory === projectCategories.frontendProjects
+                }
+              />
+
+              <Button
+                text={"backend"}
+                isPill
+                isPrimary={
+                  projectCategory !== projectCategories.backendProjects
+                }
+                onClick={() =>
+                  setProjectCategory(projectCategories.backendProjects)
+                }
+                disabled={projectCategory === projectCategories.backendProjects}
               />
             </div>
-          ))}
-        </ThreeColumnGrid>
-      </div>
-    </MainContainer>
+          </div>
+        </div>
+
+        <div className="p-spacing-default-10px">
+          <ThreeColumnGrid>
+            {project.map((project) => (
+              <div key={project.id}>
+                <ProjectCard
+                  imageSrc={project.imageSrc}
+                  imageAltText={project.imageAltText}
+                  title={project.title}
+                  link={project.link}
+                  repoName={project.repoName}
+                  description={project.description}
+                />
+              </div>
+            ))}
+          </ThreeColumnGrid>
+        </div>
+      </MainContainer>
+    </>
   );
 }
