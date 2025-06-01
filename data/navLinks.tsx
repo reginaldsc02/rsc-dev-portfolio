03e30cf1616df12abb45ff5c16f34b687187bc8f
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-06-01 15:48:42 +1200

    remove: unused icons

--- data/navLinks.tsx
+++ data/navLinks.tsx
@@ -1,39 +1,26 @@
-import {
-  BiHomeSmile,
-  BiLogoReact,
-  BiSolidBriefcase,
-  BiSolidPhone,
-  BiSolidUserAccount,
-} from "react-icons/bi";
-
 export const navLinks = [
   {
     title: "Home",
     url: "hero",
-    icon: <BiHomeSmile />,
   },
 
   {
     title: "Projects",
     url: "projects",
-    icon: <BiSolidBriefcase />,
   },
 
   {
     title: "About",
     url: "about",
-    icon: <BiSolidUserAccount />,
   },
 
   {
     title: "Skills",
     url: "skills",
-    icon: <BiLogoReact />,
   },
 
   {
     title: "Contact",
     url: "contact",
-    icon: <BiSolidPhone />,
   },
 ];
