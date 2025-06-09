5b87601b1cdade34e642fbac88da3925a16f8deb
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-06-09 21:21:33 +1200

    add: Navigation component

--- components/shared/ui/Header.tsx
+++ components/shared/ui/Header.tsx
@@ -7,6 +7,7 @@ import { BiWindowClose, BiWindowOpen } from "react-icons/bi";
 import { useState } from "react";
 import { usePathname } from "next/navigation";
 import { Logo } from "./Logo";
+import { Navigation } from "./Navigation";
 
 export const Header = () => {
   const pathname = usePathname();
@@ -30,43 +31,11 @@ export const Header = () => {
         {navDrawerOpen ? <BiWindowClose /> : <BiWindowOpen />}
       </button>
 
-      <nav
-        aria-expanded={navDrawerOpen ? true : false}
-        className={`w-full h-[calc(100vh-4rem)] md:w-auto md:h-auto pb-2.5 md:pb-0 fixed md:static top-16 left-0 z-40 md:z-auto bg-background md:bg-transparent md:translate-x-0 transition-transform overflow-y-auto ${
-          navDrawerOpen ? "translate-x-0" : "translate-x-full"
-        }`}
-      >
-        <ul className="flex flex-col md:flex-row ml-2.5">
-          {navLinks.map((nav) => (
-            <li
-              key={nav.id}
-              onClick={() => setNavDrawerOpen(!navDrawerOpen)}
-              className="p-2.5 hover:text-primary transition-colors"
-            >
-              <Link href={nav.href}>{nav.title}</Link>
-            </li>
-          ))}
-        </ul>
-
-        <ul className="mt-5 ml-5 flex gap-2.5 md:hidden">
-          {socialMediaLinks.map((sml) => (
-            <li
-              key={sml.id}
-              onClick={() => setNavDrawerOpen(!navDrawerOpen)}
-              className="hover:text-primary transition-colors"
-            >
-              <Link
-                href={sml.href}
-                aria-label={sml.title}
-                target="_blank"
-                rel="noreferrer"
-              >
-                {sml.icon}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
+      <Navigation
+        navLinks={navLinks}
+        navDrawerOpen={navDrawerOpen}
+        setNavDrawerOpen={setNavDrawerOpen}
+      />
 
       <ul className="hidden md:flex gap-2.5">
         {socialMediaLinks.map((sml) => (
---
+++ components/shared/ui/Navigation.tsx
@@ -0,0 +1,59 @@
+import Link from "next/link";
+import { socialMediaLinks } from "../lib/constants/socialMediaLinks";
+import { Dispatch, SetStateAction } from "react";
+
+interface NavLinkType {
+  id: string | number;
+  href: string;
+  title: string;
+}
+
+export const Navigation = ({
+  navLinks,
+  navDrawerOpen,
+  setNavDrawerOpen,
+}: {
+  navLinks: Array<NavLinkType>;
+  navDrawerOpen: boolean;
+  setNavDrawerOpen: Dispatch<SetStateAction<boolean>>;
+}) => {
+  return (
+    <nav
+      aria-expanded={navDrawerOpen ? true : false}
+      className={`w-full h-[calc(100vh-4rem)] md:w-auto md:h-auto pb-2.5 md:pb-0 fixed md:static top-16 left-0 z-40 md:z-auto bg-background md:bg-transparent md:translate-x-0 transition-transform overflow-y-auto ${
+        navDrawerOpen ? "translate-x-0" : "translate-x-full"
+      }`}
+    >
+      <ul className="flex flex-col md:flex-row ml-2.5">
+        {navLinks.map((nav: NavLinkType) => (
+          <li
+            key={nav.id}
+            onClick={() => setNavDrawerOpen(!navDrawerOpen)}
+            className="p-2.5 hover:text-primary transition-colors"
+          >
+            <Link href={nav.href}>{nav.title}</Link>
+          </li>
+        ))}
+      </ul>
+
+      <ul className="mt-5 ml-5 flex gap-2.5 md:hidden">
+        {socialMediaLinks.map((sml) => (
+          <li
+            key={sml.id}
+            onClick={() => setNavDrawerOpen(!navDrawerOpen)}
+            className="hover:text-primary transition-colors"
+          >
+            <Link
+              href={sml.href}
+              aria-label={sml.title}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {sml.icon}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
