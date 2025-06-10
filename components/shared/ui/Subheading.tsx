8cc289f82d580aaf1a11f508ad107c86f981eae0
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-06-10 20:48:04 +1200

    add: SubheadingType

--- components/shared/ui/Subheading.tsx
+++ components/shared/ui/Subheading.tsx
@@ -1,3 +1,7 @@
-export const Subheading = ({ text }: { text: string }) => {
+type SubheadingType = {
+  text: string;
+};
+
+export const Subheading = ({ text }: SubheadingType) => {
   return <h2 className="text-xl font-semibold text-center">{text}</h2>;
 };
