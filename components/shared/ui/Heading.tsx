fea2266787f1ee18d2eaf547e1c34a34e4fc11a9
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-06-10 21:08:57 +1200

    add: HeadingType

--- components/shared/ui/Heading.tsx
+++ components/shared/ui/Heading.tsx
@@ -1,10 +1,6 @@
-export const Heading = ({
-  text,
-  h1 = false,
-}: {
-  text: string;
-  h1?: boolean;
-}) => {
+type HeadingType = { text: string; h1?: boolean };
+
+export const Heading = ({ text, h1 = false }: HeadingType) => {
   const Tag: React.HTMLElementType = h1 ? "h1" : "div";
   return <Tag className="text-3xl font-bold text-center">{text}</Tag>;
 };
