35cd614666b0242374809dd1ceb45cd45a521ea4
Reginald Sahil Chand <reginaldsc02@outlook.com>
2025-06-10 21:23:29 +1200

    update: Header component to be more modular and suited for non-landing pages

--- components/shared/ui/Header.tsx
+++ components/shared/ui/Header.tsx
@@ -1,51 +1,43 @@
 "use client";
 
-import Link from "next/link";
-import { navLinks } from "../lib/constants/navLinks";
-import { socialMediaLinks } from "../lib/constants/socialMediaLinks";
-import { useState } from "react";
-import { usePathname } from "next/navigation";
 import { Logo } from "./Logo";
 import { Navigation } from "./Navigation";
 import { HamburgerButton } from "./HamburgerButton";
+import { ClerkProfileCard } from "./ClerkProfileCard";
+import { NavigationType } from "../lib/types/NavigationType";
+import { SocialMediaLinks } from "./SocialMediaLinks";
 
-export const Header = () => {
-  const pathname = usePathname();
-  const [navDrawerOpen, setNavDrawerOpen] = useState(false);
-
-  if (pathname.startsWith("/admin")) return null;
-
-  const handleNavDrawerOpen = () => setNavDrawerOpen(!navDrawerOpen);
-
+export const Header = ({
+  navLinks,
+  navDrawerOpen,
+  setNavDrawerOpen,
+  handleNavDrawerOpen,
+}: { handleNavDrawerOpen: () => void } & NavigationType) => {
   return (
-    <header className="min-h-16 px-5 sticky top-0 left-0 z-40 flex items-center justify-between md:justify-evenly border-b border-gray-950/10 dark:border-gray-50/10 bg-background">
-      <Logo onClick={() => setNavDrawerOpen(false)} />
+    <header className="min-h-16 px-5 sticky top-0 left-0 z-40 flex items-center justify-between border-b border-gray-950/10 dark:border-gray-50/10 bg-background">
+      <div className="flex items-center justify-center">
+        <div className="md:mr-2.5">
+          <Logo onClick={() => setNavDrawerOpen(false)} />
+        </div>
+
+        <span className="hidden md:flex">|</span>
+
+        <Navigation
+          navLinks={navLinks}
+          navDrawerOpen={navDrawerOpen}
+          setNavDrawerOpen={setNavDrawerOpen}
+        />
+      </div>
 
       <HamburgerButton
         navDrawerOpen={navDrawerOpen}
         handleNavDrawerOpen={handleNavDrawerOpen}
       />
 
-      <Navigation
-        navLinks={navLinks}
-        navDrawerOpen={navDrawerOpen}
-        setNavDrawerOpen={setNavDrawerOpen}
-      />
-
-      <ul className="hidden md:flex gap-2.5">
-        {socialMediaLinks.map((sml) => (
-          <li key={sml.id} className="hover:text-primary transition-colors">
-            <Link
-              href={sml.href}
-              aria-label={sml.title}
-              target="_blank"
-              rel="noreferrer"
-            >
-              {sml.icon}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <div className="hidden md:flex items-center justify-center gap-5">
+        <SocialMediaLinks />
+        <ClerkProfileCard />
+      </div>
     </header>
   );
 };
